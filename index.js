9d5fc4d916950595fac5b38ddfb12c1d489cdc33
Andres Mora <candresmoraa@gmail.com>
2022-06-13 20:54:16 -0500

    class Busqueda added

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-const { inquirerMenu, pausa } = require("./helpers/inquirer")
+const { inquirerMenu, pausa, leerInput } = require("./helpers/inquirer");
+const Busquedas = require('./models/busquedas');
 
 const main = async() => {
     
+    const busquedas = new Busquedas();
+
     console.clear();
 
     let opt;
 
     do {
         opt = await inquirerMenu(); 
-        await pausa();
+
+        switch (opt) {
+            case 1:
+            
+            const lugar = await leerInput('Ciudad: ');
+            console.log(lugar);
+            
+            console.log('\nInformacion de la ciudad\n'.green);
+            console.log('Ciudad:', )
+            console.log('Lat:', )
+            console.log('Lng:', )
+            console.log('Temperatura:', )
+            break;
+            
+        }
+
+        if( opt !== 0 ) await pausa();
+        
     } while ( opt !== 0 );
 
 }
---
+++ models/busquedas.js
@@ -0,0 +1,21 @@
+
+
+
+class Busquedas {
+
+    historial = ['Bogota', 'Madrid', 'San Jose'];
+
+    constructor() {
+        // TODO: if DB exists, read the DB
+    }
+
+    async ciudad( lugar = '' ) {
+        console.log( lugar );
+
+        return [];
+    }
+
+
+}
+
+module.exports = Busquedas;
